e78b9c9a15a0bfb119b413ea9b61c7f18fb324bb
celiaxz <93530463+Celiaxz@users.noreply.github.com>
2023-07-06 15:29:55 +0200

    update ironbeers

--- app.js
+++ app.js
@@ -11,12 +11,41 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(expressLayouts);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      res.render('beers', { data: beersFromApi });
+      console.log('Beers from the database: ', beersFromApi);
+    })
+    .catch(error => console.log(error));
+});
+app.get('/random_beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(beersFromApi => {
+      res.render('random_beer', { beer: beersFromApi[0] });
+      console.log('Random beer from the database:', beersFromApi[0]);
+    })
+    .catch(error => console.log(error));
+});
 
+app.get('/beers/:id', (req, res) => {
+  const beerId = req.params.id;
+  console.log('beerId: ', beerId);
+  punkAPI
+    .getBeer(beerId)
+    .then(beerArray => {
+      res.render('partials/beer', { beer: beerArray[0] });
+      console.log('Beer details:', beerArray[0]);
+    })
+    .catch(error => console.log(error));
+});
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
